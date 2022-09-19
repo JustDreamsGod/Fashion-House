3cd15e1a5abd6f82bb970bc33b239592b6999530
JustDreamsGod <73347054+JustDreamsGod@users.noreply.github.com>
2022-09-19 22:26:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* BACK TO TOP BUTTON */
+
 $(window).scroll(function () {
     let scrolled = $(window).scrollTop();
     if (scrolled > 100) {
@@ -19,5 +21,21 @@ $(window).scroll(function () {
 });
 $('#back_to_top').click(function () {
     $('body, html').animate({ scrollTop: 0 }, 1000)
+});
+
+/* BURGER */
+
+$(document).ready(function () {
+    $('.menu-burger__header').click(function () {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__menu').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+    });
+});
+
+$(document).on('click', 'a', function (event) {
+
+    event.preventDefault();
+
 });
 
